6ea3b807842fda4940d1e16ce78c8bd5d7898477
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-28 16:02:52 +0200

    Fix tests

--- tests/CloudCrafter.FunctionalTests/ApiEndpoints/ContributorList.cs
+++ tests/CloudCrafter.FunctionalTests/ApiEndpoints/ContributorList.cs
@@ -8,29 +8,29 @@ using Xunit;
 namespace CloudCrafter.FunctionalTests.ApiEndpoints;
 
 using static Testing;
+
 public class ContributorList : BaseTestFixture
 {
-  private readonly HttpClient _client = _factory.CreateClient();
+    private readonly HttpClient _client = _factory.CreateClient();
 
-  [OneTimeTearDown]
-  public void TearDown()
-  {
-      _client?.Dispose();
-  }
-  
-  [Test]
-  public async Task ReturnsTwoContributors()
-  {
-      HttpResponseMessage async = await _client.GetAsync("/Contributors");
-      string json = await async.Content.ReadAsStringAsync();
-    var result = await _client.GetAndDeserializeAsync<ContributorListResponse>("/Contributors");
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        _client?.Dispose();
+    }
 
-    Assert.Equals(2, result.Contributors.Count);
+    [Test]
+    public async Task ReturnsTwoContributors()
+    {
+        HttpResponseMessage async = await _client.GetAsync("/Contributors");
+        string json = await async.Content.ReadAsStringAsync();
+        var result = await _client.GetAndDeserializeAsync<ContributorListResponse>("/Contributors");
 
-    result.Contributors.Should()
-        .Satisfy(x => x.Name == SeedData.Contributor1.Name);
-    
-    result.Contributors.Should()
-        .Satisfy(x => x.Name == SeedData.Contributor2.Name);
-  }
+        result.Contributors.Count.Should().Be(2);
+
+        result.Contributors.Any(x => x.Name == SeedData.Contributor1.Name).Should().BeTrue();
+
+
+        result.Contributors.Any(x => x.Name == SeedData.Contributor2.Name).Should().BeTrue();
+    }
 }
