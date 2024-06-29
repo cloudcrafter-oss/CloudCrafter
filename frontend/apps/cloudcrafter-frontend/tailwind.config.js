4d6630c622d1fc7bb665191d5f8fe850172cf7f1
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-29 14:23:49 +0200

    WIP TailwindCSS

--- frontend/apps/cloudcrafter-frontend/tailwind.config.js
+++ frontend/apps/cloudcrafter-frontend/tailwind.config.js
@@ -1,17 +1,9 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
