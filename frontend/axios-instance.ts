ea5958db2b64646c336f074a8dcf605a3aea4f78
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-29 16:03:03 +0200

    Just go plain typescript react

--- frontend/axios-instance.ts
+++ frontend/axios-instance.ts
@@ -4,23 +4,23 @@ import qs from 'qs';
 // https://orval.dev/reference/configuration/output#mutator
 
 const customOptions: AxiosRequestConfig = {
-  paramsSerializer: {
-    // Override the params serialization so that complex object arrays in query strings are properly
-    // received by the backend.
-    serialize: params => qs.stringify(params, { allowDots: true }),
-  },
+    paramsSerializer: {
+        // Override the params serialization so that complex object arrays in query strings are properly
+        // received by the backend.
+        serialize: params => qs.stringify(params, { allowDots: true }),
+    },
 };
 
 // add a second `options` argument here if you want to pass extra options to each generated query
 export const axiosInstance = async <T>(
-  config: AxiosRequestConfig,
-  options?: AxiosRequestConfig,
+    config: AxiosRequestConfig,
+    options?: AxiosRequestConfig,
 ): Promise<T> => {
-  const result = await axios({
-    ...config,
-    ...customOptions,
-    ...options,
-  });
+    const result = await axios({
+        ...config,
+        ...customOptions,
+        ...options,
+    });
 
-  return result.data;
+    return result.data;
 };
--- frontend/libs/plugins/codegen/src/lib/plugins-codegen.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pluginsCodegen } from './plugins-codegen';
-
-describe('pluginsCodegen', () => {
-  it('should work', () => {
-    expect(pluginsCodegen()).toEqual('plugins-codegen');
-  });
-});
