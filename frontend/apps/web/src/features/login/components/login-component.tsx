d84f59cc92cb26da5a93dcac975ec427ebc81d23
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-30 11:51:14 +0200

    Default values

--- frontend/apps/web/src/features/login/components/login-component.tsx
+++ frontend/apps/web/src/features/login/components/login-component.tsx
@@ -35,46 +35,48 @@ export const LoginComponent = () => {
         }
     })
 
-    const handleLogin = () => {
-        login.mutate({
-            email: '',
-            password: ''
-        })
-
+    const handleLogin = (values: z.infer<typeof postLoginUserMutationRequestSchema>) => {
+        login.mutate(values)
     }
 
     const form = useForm<z.infer<typeof postLoginUserMutationRequestSchema>>({
         resolver: zodResolver(postLoginUserMutationRequestSchema),
         defaultValues: {
-            email: '',
-            password: ''
+            email: 'admin@admin.com',
+            password: 'P@ssw0rd!123'
         }
     })
 
     return (
         <Form {...form}>
-            <Card className="w-full max-w-sm">
-                <CardHeader>
-                    <CardTitle className="text-2xl">Login</CardTitle>
-                    <CardDescription>Enter your email below to login to your account.</CardDescription>
-                </CardHeader>
-                <CardContent className="grid gap-4">
-                    <FormField render={({ field }) => (
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input {...field} placeholder="m@example.com"/>
-                        </div>
-                    )} control={form.control}
-                               name={'email'}/>
-                    <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" required/>
-                    </div>
-                </CardContent>
-                <CardFooter>
-                    <Button onClick={handleLogin} className="w-full">Sign in</Button>
-                </CardFooter>
-            </Card>
+            <form onSubmit={form.handleSubmit(handleLogin)}>
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle className="text-2xl">Login</CardTitle>
+                        <CardDescription>Enter your email below to login to your account.</CardDescription>
+                    </CardHeader>
+                    <CardContent className="grid gap-4">
+                        <FormField render={({ field }) => (
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input {...field} placeholder="m@example.com"/>
+                            </div>
+                        )} control={form.control}
+                                   name={'email'}/>
+
+                        <FormField render={({ field }) => (
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Password</Label>
+                                <Input {...field} id="password" type="password"/>
+                            </div>
+                        )} control={form.control}
+                                   name={'password'}/>
+                    </CardContent>
+                    <CardFooter>
+                        <Button type={'submit'} className="w-full">Sign in</Button>
+                    </CardFooter>
+                </Card>
+            </form>
         </Form>
     )
 }
