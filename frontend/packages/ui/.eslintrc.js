ec76b846f7ad1e3cd659f5deea6093ccf521d782
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-30 09:48:44 +0200

    Eslint fixer

--- frontend/packages/ui/.eslintrc.js
+++ frontend/packages/ui/.eslintrc.js
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
+    root: true,
+    extends: ["@repo/eslint-config/index.js"],
 };
