9f61778e877bbb73d4a0e6c07d4ca505d50f2c54
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-30 18:11:37 +0200

    Removed old use cases

--- backend/src/CloudCrafter.UseCases/Contributors/List/ListContributorsQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using Ardalis.Result;
-using Ardalis.SharedKernel;
-
-namespace CloudCrafter.UseCases.Contributors.List;
-
-public record ListContributorsQuery(int? Skip, int? Take) : IQuery<Result<IEnumerable<ContributorDTO>>>;
