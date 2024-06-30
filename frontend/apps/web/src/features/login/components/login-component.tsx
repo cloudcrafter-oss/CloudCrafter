8b229eac9398e9a56da226259765ce585d927e3f
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-30 11:47:25 +0200

    Email from useForm

--- frontend/apps/web/src/features/login/components/login-component.tsx
+++ frontend/apps/web/src/features/login/components/login-component.tsx
@@ -6,11 +6,16 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
+    Form,
+    FormField,
     Input,
     useToast
 } from '@ui/components'
 import { Label } from '@ui/components/ui/label.tsx'
-import { usePostLoginUserHook } from '../../../app/generated'
+import { postLoginUserMutationRequestSchema, usePostLoginUserHook } from '../../../app/generated'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export const LoginComponent = () => {
 
@@ -38,25 +43,38 @@ export const LoginComponent = () => {
 
     }
 
+    const form = useForm<z.infer<typeof postLoginUserMutationRequestSchema>>({
+        resolver: zodResolver(postLoginUserMutationRequestSchema),
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    })
+
     return (
-        <Card className="w-full max-w-sm">
-            <CardHeader>
-                <CardTitle className="text-2xl">Login</CardTitle>
-                <CardDescription>Enter your email below to login to your account.</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-                <div className="grid gap-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="m@example.com" required/>
-                </div>
-                <div className="grid gap-2">
-                    <Label htmlFor="password">Password</Label>
-                    <Input id="password" type="password" required/>
-                </div>
-            </CardContent>
-            <CardFooter>
-                <Button onClick={handleLogin} className="w-full">Sign in</Button>
-            </CardFooter>
-        </Card>
+        <Form {...form}>
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>Enter your email below to login to your account.</CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <FormField render={({ field }) => (
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input {...field} placeholder="m@example.com"/>
+                        </div>
+                    )} control={form.control}
+                               name={'email'}/>
+                    <div className="grid gap-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" type="password" required/>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button onClick={handleLogin} className="w-full">Sign in</Button>
+                </CardFooter>
+            </Card>
+        </Form>
     )
 }
