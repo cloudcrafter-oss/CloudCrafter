4fac9c2c1039c6be1341e8d53d445ef91c535c09
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-07-01 18:17:10 +0200

    Add NextJS

---
+++ frontend/apps/web-nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Button } from '@ui/components'
+
+export default function Page() {
+    return (
+        <main>
+            <Button>Click me</Button>
+        </main>
+    )
+}
