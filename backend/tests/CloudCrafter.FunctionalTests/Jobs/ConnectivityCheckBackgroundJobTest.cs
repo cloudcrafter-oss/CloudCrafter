52d73d6e906e4a48058a1f6e12af3dae931747f2
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-07-14 12:42:44 +0200

    Test unhealthy server

--- backend/tests/CloudCrafter.FunctionalTests/Jobs/ConnectivityCheckBackgroundJobTest.cs
+++ backend/tests/CloudCrafter.FunctionalTests/Jobs/ConnectivityCheckBackgroundJobTest.cs
@@ -22,9 +22,12 @@ public class ConnectivityCheckBackgroundJobTest : BaseTestFixture
     {
         (await CountAsync<BackgroundJob>()).Should().Be(0);
         (await CountAsync<ServerConnectivityCheckJob>()).Should().Be(0);
-        var server = TestingHostServerFaker().Generate();
         (await CountAsync<Server>()).Should().Be(0);
+        
+        
+        var server = TestingHostServerFaker().Generate();
         await AddAsync(server);
+        
         (await CountAsync<Server>()).Should().Be(1);
         var dispatcher = GetService<ICloudCrafterDispatcher>();
 
@@ -34,9 +37,7 @@ public class ConnectivityCheckBackgroundJobTest : BaseTestFixture
 
         (await CountAsync<BackgroundJob>()).Should().Be(1);
         (await CountAsync<ServerConnectivityCheckJob>()).Should().Be(1);
-
-        var ctx = GetService<IApplicationDbContext>();
-
+        
         var job = FetchEntity<BackgroundJob>(x => x.HangfireJobId == jobId,
             f => f.Include(x => x.ServerConnectivityCheckJob)
                 .ThenInclude(x => x != null ? x.Server : null));
@@ -52,6 +53,46 @@ public class ConnectivityCheckBackgroundJobTest : BaseTestFixture
 
         job.RunningTime.Should().BeGreaterThan(1);
         job.ServerConnectivityCheckJob.TimeTakenMs.Should().BeGreaterThan(1);
+        job.Logs.Where(x => string.IsNullOrEmpty(x.Exception)).ToList().Count.Should().Be(0);
         job.Logs.Count.Should().BeGreaterThan(0);
+    }
+
+    [Test]
+    public async Task ShouldBeAbleToSuccessfullyRunConnectivityJobButUnhealthy()
+    {
+        (await CountAsync<BackgroundJob>()).Should().Be(0);
+        (await CountAsync<ServerConnectivityCheckJob>()).Should().Be(0);
+        (await CountAsync<Server>()).Should().Be(0);
+        
+        
+        var server = FakerInstances.ServerFaker.Generate();
+        await AddAsync(server);
+        
+        (await CountAsync<Server>()).Should().Be(1);
+        var dispatcher = GetService<ICloudCrafterDispatcher>();
+
+        var jobId = await dispatcher.EnqueueConnectivityCheck(server);
+
+        WaitForJobCompletion(jobId);
+
+        (await CountAsync<BackgroundJob>()).Should().Be(1);
+        (await CountAsync<ServerConnectivityCheckJob>()).Should().Be(1);
+        
+        var job = FetchEntity<BackgroundJob>(x => x.HangfireJobId == jobId,
+            f => f.Include(x => x.ServerConnectivityCheckJob)
+                .ThenInclude(x => x != null ? x.Server : null));
+
+        job.Should().NotBeNull();
+        job!.ServerConnectivityCheckJob.Should().NotBeNull();
+        job.ServerConnectivityCheckJob!.Server.Should().NotBeNull();
+        job.ServerConnectivityCheckJob.ServerId.Should().Be(server.Id);
+        job.ServerConnectivityCheckJob.Server.Name.Should().Be(server.Name);
+        
+        job.Status.Should().Be(BackgroundJobStatus.Completed);
+        job.ServerConnectivityCheckJob.Result.Should().Be(ServerConnectivityCheckResult.Unhealthy);
+
+        job.RunningTime.Should().BeGreaterThan(1);
+        job.ServerConnectivityCheckJob.TimeTakenMs.Should().BeGreaterThan(1);
+        job.Logs.Where(x => string.IsNullOrEmpty(x.Exception)).ToList().Count.Should().BeGreaterThan(0);
     }
 }
