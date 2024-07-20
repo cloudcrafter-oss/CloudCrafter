bcd43c72ab828480f73e60805df3c13dae438677
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-07-20 09:34:11 +0200

    Integration test agent

--- backend/src/CloudCrafter.Agent.Console/Program.cs
+++ backend/src/CloudCrafter.Agent.Console/Program.cs
@@ -1,50 +1,84 @@
-﻿using CloudCrafter.Agent.Console.Commands;
+﻿using System.CommandLine;
+using CloudCrafter.Agent.Console.Commands;
+using CloudCrafter.Agent.Models.Recipe;
 using CloudCrafter.Agent.Runner;
 using CloudCrafter.Agent.Runner.Cli;
 using CloudCrafter.Agent.Runner.DeploymentLogPump;
 using CloudCrafter.Agent.Runner.DeploymentLogPump.Implementation;
-using CloudCrafter.Agent.Runner.IO;
 using CloudCrafter.Agent.Runner.RunnerEngine.Deployment;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-static IHostBuilder CreateHostBuilder(string[] args)
+public class Program
 {
-    return Host.CreateDefaultBuilder(args)
-        .ConfigureServices((hostContext, services) =>
+    public static async Task<int> Main(string[] args)
+    {
+        return await RunApp(args);
+    }
+
+    public static async Task ValidateAndDeployRecipe(IHost host, DeploymentRecipe recipe)
+    {
+        var deploymentService = host.Services.GetRequiredService<DeploymentService>();
+
+        await deploymentService.ValidateRecipe(recipe);
+
+        await deploymentService.DeployAsync(recipe);
+    }
+
+    public static async Task<int> RunApp(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        var demoOption = new Option<bool>("--demo", "Runs the dummy deployment recipe");
+
+        var rootCommand = new RootCommand("CloudCrafter Agent");
+        rootCommand.AddOption(demoOption);
+
+        int? exitCode = null;
+
+        rootCommand.SetHandler(async demoOptionValue =>
         {
-            services.AddDeploymentStepsConfig();
-            // services.AddTransient<IDeploymentStepHandler<GitCheckoutParams>, CheckoutGitRepositoryStepHandler>();
-            // services.AddTransient<IDeploymentStepConfig<GitCheckoutParams>, GitCheckoutConfig>();
-            //
-            services.AddSingleton<IMessagePump, MessagePump>();
-            services.AddTransient<ICommandExecutor, CommandExecutor>();
-            services.AddSingleton<IDeploymentStepFactory, DeploymentStepFactory>();
+            var mediator = host.Services.GetRequiredService<IMediator>();
 
-            services.AddTransient<DeploymentService>();
-            services.AddMediatR(cfg =>
+            DeploymentRecipe? recipe = null;
+            if (demoOptionValue)
             {
-                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
-                cfg.RegisterServicesFromAssembly(typeof(IAgentRunnerTarget).Assembly);
+                recipe = await mediator.Send(new GetDummyDeployment.Query());
+            }
+
+            if (recipe == null)
+            {
+                exitCode = -1;
+                Console.WriteLine("Error: No Recipe found - cannot continue");
+                return;
+            }
+
+            await ValidateAndDeployRecipe(host, recipe);
+        }, demoOption);
+
+
+        var intResult = await rootCommand.InvokeAsync(args);
+
+        return exitCode ?? intResult;
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddDeploymentStepsConfig();
+                services.AddSingleton<IMessagePump, MessagePump>();
+                services.AddTransient<ICommandExecutor, CommandExecutor>();
+                services.AddSingleton<IDeploymentStepFactory, DeploymentStepFactory>();
+
+                services.AddTransient<DeploymentService>();
+                services.AddMediatR(cfg =>
+                {
+                    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+                    cfg.RegisterServicesFromAssembly(typeof(IAgentRunnerTarget).Assembly);
+                });
             });
-        });
+    }
 }
-
-
-var host = CreateHostBuilder(args).Build();
-
-var mediator = host.Services.GetRequiredService<IMediator>();
-
-var recipe = await mediator.Send(new GetDummyDeployment.Query());
-var deploymentService = host.Services.GetRequiredService<DeploymentService>();
-
-await deploymentService.ValidateRecipe(recipe);
-
-await deploymentService.DeployAsync(recipe);
-
-var writer = new YamlRecipeWriter(recipe);
-
-var yaml = writer.WriteString();
-Console.Write(yaml);
-var pump = host.Services.GetRequiredService<IMessagePump>();
---
+++ backend/tests/CloudCrafter.Agent.Console.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+
+namespace CloudCrafter.Agent.Console.IntegrationTests;
+
+public class IntegrationTest
+{
+    [Test]
+    public async Task ShouldBuildDemoDeployment()
+    {
+        string[] args = [];
+
+        using var sw = new StringWriter();
+        System.Console.SetOut(sw);
+
+        var exitCode = await Program.Main(args);
+
+        exitCode.Should().Be(-1);
+
+        var output = sw.ToString();
+        output.Should().Contain("Error: No Recipe found - cannot continue");
+    }
+}
