7cfd7956b213749619469044d2f950b1f3264e5d
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-07-24 20:46:06 +0200

    Add recipe reader tests

---
+++ backend/src/CloudCrafter.Agent.Runner/IO/YamlRecipeReader.cs
@@ -0,0 +1,46 @@
+﻿using CloudCrafter.Agent.Models.Recipe;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CloudCrafter.Agent.Runner.IO;
+
+public class YamlRecipeReader
+{
+    public DeploymentRecipe FromBase64(string base64)
+    {
+        var yaml = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(base64));
+
+        return FromString(yaml);
+    }
+
+    public DeploymentRecipe FromString(string yaml)
+    {
+        try
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            var recipe = deserializer.Deserialize<DeploymentRecipe>(yaml);
+
+
+            if (recipe == null)
+            {
+                throw new InvalidOperationException("Deserialization resulted in a null object.");
+            }
+
+
+            return recipe;
+        }
+        catch (YamlException ex)
+        {
+            throw new InvalidOperationException("Failed to deserialize YAML: " + ex.Message, ex);
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("An unexpected error occurred while deserializing YAML: " + ex.Message,
+                ex);
+        }
+    }
+}
---
+++ backend/tests/CloudCrafter.Agent.Runner.Tests/Fakers/AgentFakers.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using CloudCrafter.Agent.Models.Recipe;
+
+namespace CloudCrafter.Agent.Runner.Tests.Fakers;
+
+public abstract class AgentFakers
+{
+    public static Faker<DeploymentRecipe> DeploymentRecipeFaker() => new Faker<DeploymentRecipe>()
+        .StrictMode(true)
+        .RuleFor(x => x.Name, f => $"Application stack {f.Random.Number()}")
+        .RuleFor(x => x.DockerComposeOptions, opt => null)
+        .RuleFor(x => x.Destination, f => DeploymentRecipeDestinationFaker().Generate())
+        .RuleFor(x => x.BuildOptions, f => DeploymentBuildOptionsFaker().Generate());
+
+
+    public static Faker<DeploymentRecipeDestination> DeploymentRecipeDestinationFaker() =>
+        new Faker<DeploymentRecipeDestination>()
+            .StrictMode(true)
+            .RuleFor(x => x.RootDirectory, f => f.System.DirectoryPath());
+
+    public static Faker<DeploymentBuildOptions> DeploymentBuildOptionsFaker() =>
+        new Faker<DeploymentBuildOptions>()
+            .StrictMode(true)
+            .RuleFor(x => x.Steps, f => new List<DeploymentBuildStep>());
+}
