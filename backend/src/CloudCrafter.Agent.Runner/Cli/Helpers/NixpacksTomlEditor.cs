836872d7295df255bafe79f22a340701c7297f77
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-08-10 11:04:09 +0200

    Add variables for toml editor

--- backend/src/CloudCrafter.Agent.Runner/Cli/Helpers/NixpacksTomlEditor.cs
+++ backend/src/CloudCrafter.Agent.Runner/Cli/Helpers/NixpacksTomlEditor.cs
@@ -1,4 +1,6 @@
-﻿using Tomlyn;
+﻿using System.Text;
+using Tomlyn;
+using Tomlyn.Syntax;
 using Tomlyn.Model;
 
 namespace CloudCrafter.Agent.Runner.Cli.Helpers;
@@ -12,6 +14,22 @@ public class NixpacksTomlEditor
         _model = Toml.ToModel(toml);
     }
 
+    public void AddVariables(IDictionary<string, string> variables)
+    {
+        if (!_model.ContainsKey("variables"))
+        {
+            _model.Add("variables", new TomlTable());
+        }
+
+        var variablesTable = (TomlTable)_model["variables"];
+
+        foreach (var variable in variables)
+        {
+            variablesTable[variable.Key] = variable.Value;
+        }
+    }
+
+   
     public void AddPackages(IEnumerable<string> packages)
     {
         var phasesTable = (TomlTable)_model["phases"];
@@ -45,6 +63,44 @@ public class NixpacksTomlEditor
 
     public string GetToml()
     {
-        return Toml.FromModel(_model);
+        return CustomTomlSerializer.Serialize(_model);
     }
 }
+
+
+public static class CustomTomlSerializer
+{
+    public static string Serialize(TomlTable table)
+    {
+        var sb = new StringBuilder();
+        SerializeTable(table, sb, 0);
+        return sb.ToString();
+    }
+
+    private static void SerializeTable(TomlTable table, StringBuilder sb, int indent)
+    {
+        foreach (var kvp in table)
+        {
+            if (kvp.Value is TomlTable nestedTable)
+            {
+                sb.AppendLine($"{new string(' ', indent)}[{kvp.Key}]");
+                SerializeTable(nestedTable, sb, indent + 2);
+            }
+            else
+            {
+                sb.AppendLine($"{new string(' ', indent)}{kvp.Key} = {SerializeValue(kvp.Value)}");
+            }
+        }
+    }
+
+    private static string SerializeValue(object? value)
+    {
+        return value switch
+        {
+            string str => $"'{str}'",
+            TomlArray arr => $"[{string.Join(", ", arr.Select(SerializeValue))}]",
+            _ => value?.ToString() ?? "null"
+        };
+    }
+}
+
