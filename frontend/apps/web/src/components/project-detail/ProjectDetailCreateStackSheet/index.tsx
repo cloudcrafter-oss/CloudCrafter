b28c620e1ea390f75c8eee1e836afb5fa28fe842
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-08-31 18:31:55 +0200

    Adding validation

--- frontend/apps/web/src/components/project-detail/ProjectDetailCreateStackSheet/index.tsx
+++ frontend/apps/web/src/components/project-detail/ProjectDetailCreateStackSheet/index.tsx
@@ -1,6 +1,6 @@
+'use client'
 import { Button } from '@ui/components/ui/button'
 import { Input } from '@ui/components/ui/input'
-import { Label } from '@ui/components/ui/label'
 import {
 	Sheet,
 	SheetContent,
@@ -10,8 +10,36 @@ import {
 	SheetTrigger,
 } from '@ui/components/ui/sheet'
 import { Plus } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@ui/components/ui/form'
+
+const formSchema = z.object({
+	gitRepository: z.string().url('Please enter a valid URL'),
+})
 
 export const ProjectDetailCreateStackSheet = () => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			gitRepository: '',
+		},
+	})
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Handle form submission
+		console.log(values)
+	}
+
 	return (
 		<Sheet>
 			<SheetTrigger asChild>
@@ -20,24 +48,34 @@ export const ProjectDetailCreateStackSheet = () => {
 					<Plus className='ml-2 h-4 w-4' />
 				</Button>
 			</SheetTrigger>
-			<SheetContent>
+			<SheetContent style={{ maxWidth: '33vw' }}>
 				<SheetHeader>
 					<SheetTitle>Deploy new Stack</SheetTitle>
 					<SheetDescription>
 						Enter the details for your new Stack.
 					</SheetDescription>
 				</SheetHeader>
-				<div className='grid gap-4 py-4'>
-					<div className='grid grid-cols-4 items-center gap-4'>
-						<Label htmlFor='github-url' className='text-right'>
-							GitHub Repository (Public)
-						</Label>
-						<Input id='github-url' className='col-span-3' />
-					</div>
-				</div>
-				<div className='mt-4 flex justify-end'>
-					<Button type='submit'>Add Stack</Button>
-				</div>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+						<FormField
+							control={form.control}
+							name='gitRepository'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Git Repository (Public)</FormLabel>
+									<FormControl>
+										<Input {...field} autoComplete='off' />
+									</FormControl>
+									<FormDescription>
+										Enter the URL of your public Git repository.
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type='submit'>Add Stack</Button>
+					</form>
+				</Form>
 			</SheetContent>
 		</Sheet>
 	)
