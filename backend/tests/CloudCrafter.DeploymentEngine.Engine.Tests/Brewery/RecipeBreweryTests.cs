adec0cce6fe80e62d10ff3ad89ab88b9c3af9504
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-09-06 22:31:39 +0200

    Add verify files

--- backend/tests/CloudCrafter.DeploymentEngine.Engine.Tests/Brewery/RecipeBreweryTests.cs
+++ backend/tests/CloudCrafter.DeploymentEngine.Engine.Tests/Brewery/RecipeBreweryTests.cs
@@ -1,4 +1,5 @@
-﻿using CloudCrafter.DeploymentEngine.Engine.Brewery;
+﻿using CloudCrafter.Agent.Runner.IO;
+using CloudCrafter.DeploymentEngine.Engine.Brewery;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -28,5 +29,22 @@ public class RecipeBreweryTests
         brewery.SetApplication(Guid.NewGuid());
         var act = () => brewery.Build();
         act.Should().Throw<InvalidOperationException>().WithMessage("Destination must be set.");
+    }
+
+    [Test]
+    public async Task ShouldBeAbleToBuildRecipe()
+    {
+        var brewery = new RecipeBrewery("Test");
+        var guid = Guid.Parse("f4ccb2c1-0ce2-4ea7-9129-9b6755a1fad2");
+        brewery.SetApplication(guid);
+        brewery.SetDestination("/tmp/cloudcrafter");
+        var recipe = brewery.Build();
+        recipe.Should().NotBeNull();
+
+        var recipeWriter = new YamlRecipeWriter(recipe);
+
+        var yaml = recipeWriter.WriteString();
+
+        await Verify(yaml);
     }
 }
