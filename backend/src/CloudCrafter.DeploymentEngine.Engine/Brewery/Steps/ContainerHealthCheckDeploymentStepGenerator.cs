e7d37edcaccb60f8e232768f4911176c9cf5c9c0
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-09-07 12:13:46 +0200

    Add more tests

---
+++ backend/src/CloudCrafter.DeploymentEngine.Engine/Brewery/Steps/ContainerHealthCheckDeploymentStepGenerator.cs
@@ -0,0 +1,69 @@
+﻿using CloudCrafter.Agent.Models.Recipe;
+using CloudCrafter.DeploymentEngine.Engine.Abstraction;
+
+namespace CloudCrafter.DeploymentEngine.Engine.Brewery.Steps;
+
+public class ContainerHealthCheckDeploymentStepGenerator(ContainerHealthCheckDeploymentStepGenerator.Args options)
+    : IBuildStepGenerator
+{
+    public DeploymentBuildStep Generate()
+    {
+
+        var services = options.Services.ToDictionary(service => service.Key, service => GenerateServiceCheck(service.Value));
+
+        return new DeploymentBuildStep
+        {
+            Name = "Container health check",
+            Description = "Checks the health of the containers",
+            Type = DeploymentBuildStepType.ContainerHealthCheck,
+            Params = new Dictionary<string, object>
+            {
+                {
+                    "dockerComposeSettings", new Dictionary<string, object>
+                    {
+                        { "fetchServicesFromContext", options.DockerComposeSettings.FetchServicesFromContext }
+                    }
+                },
+                {
+                    "services", services
+                }
+            }
+        };
+    }
+
+    private Dictionary<string, object> GenerateServiceCheck(ArgsHealthCheckSettings settings)
+    {
+        return new Dictionary<string, object>
+        {
+            { "httpMethod", settings.HttpMethod },
+            { "httpSchema", settings.HttpSchema },
+            { "httpHost", settings.HttpHost },
+            { "httpPath", settings.HttpPath },
+            { "httpPort", settings.HttpPort },
+            { "expectedResponseCode", settings.ExpectedHttpStatusCode },
+            { "retries", settings.MaxRetries }
+        };
+    }
+
+    public class Args
+    {
+        public required ArgsComposeSettings DockerComposeSettings { get; init; }
+        public required Dictionary<string, ArgsHealthCheckSettings> Services { get; init; }
+    }
+
+    public class ArgsComposeSettings
+    {
+        public required bool FetchServicesFromContext { get; init; }
+    }
+
+    public class ArgsHealthCheckSettings
+    {
+        public required string HttpMethod { get; init; }
+        public required string HttpSchema { get; init; }
+        public required string HttpHost { get; init; }
+        public required string HttpPath { get; init; }
+        public required int HttpPort { get; init; }
+        public required int ExpectedHttpStatusCode { get; init; }
+        public required int MaxRetries { get; init; }
+    }
+}
---
+++ backend/src/CloudCrafter.DeploymentEngine.Engine/Brewery/Steps/WriteDockerComposeBuildStepGenerator.cs
@@ -0,0 +1,24 @@
+﻿using CloudCrafter.Agent.Models.Recipe;
+using CloudCrafter.DeploymentEngine.Engine.Abstraction;
+
+namespace CloudCrafter.DeploymentEngine.Engine.Brewery.Steps;
+
+public class WriteDockerComposeBuildStepGenerator(WriteDockerComposeBuildStepGenerator.Args options)
+    : IBuildStepGenerator
+{
+    public DeploymentBuildStep Generate()
+    {
+        return new DeploymentBuildStep
+        {
+            Name = "Write Docker Compose",
+            Description = "Writes the Docker Compose file to the filesystem",
+            Type = DeploymentBuildStepType.DockerComposeWriteToFileSystem,
+            Params = new Dictionary<string, object> { { "dockerComposeFile", options.DockerComposeFileName } }
+        };
+    }
+
+    public class Args
+    {
+        public required string DockerComposeFileName { get; init; }
+    }
+}
