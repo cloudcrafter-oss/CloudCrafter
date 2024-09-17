ca90cf969d28075e9795427ece27398130bc0162
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-09-17 18:25:02 +0200

    Fix tests

--- backend/tests/CloudCrafter.DeploymentEngine.Remote.IntegrationTests/CloudCraftManagerTest.cs
+++ backend/tests/CloudCrafter.DeploymentEngine.Remote.IntegrationTests/CloudCraftManagerTest.cs
@@ -83,6 +83,6 @@ public class CloudCraftManagerTest
         result.Result.Should().BeEmpty();
         result
             .Error.Should()
-            .Be("bash: line 1: /some/non/existing/directory/file.txt: No such file or directory");
+            .Be("sh: can't create /some/non/existing/directory/file.txt: nonexistent directory");
     }
 }
