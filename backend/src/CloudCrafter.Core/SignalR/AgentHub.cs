0e0c5ed5689a9dd665a9707b4c8fa732b72ed1e2
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-02 19:55:16 +0200

    Abstracted some stuff away

--- backend/src/CloudCrafter.Core/SignalR/AgentHub.cs
+++ backend/src/CloudCrafter.Core/SignalR/AgentHub.cs
@@ -1,8 +1,6 @@
 ﻿using CloudCrafter.Agent.SignalR;
 using CloudCrafter.Agent.SignalR.Models;
 using CloudCrafter.Core.Interfaces.Domain.Servers;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 
 namespace CloudCrafter.Core.SignalR;
@@ -11,7 +9,7 @@ public class AgentHub(
     IServersService serversService,
     ILogger<AgentHub> logger,
     ConnectedServerManager serverManager
-) : Hub, IAgentHub
+) : CloudCrafterBaseHub(serversService, logger, serverManager), IAgentHub
 {
     public Task HealthCheckCommand(HealthCheckCommandArgs args)
     {
@@ -24,88 +22,5 @@ public class AgentHub(
         );
 
         return Task.CompletedTask;
-    }
-
-    private async Task<Guid> GetServerForClient()
-    {
-        var connectionId = Context.ConnectionId;
-        var serverId = await serverManager.GetServerIdForConnectionId(connectionId);
-
-        if (serverId.HasValue)
-        {
-            return serverId.Value;
-        }
-
-        throw new InvalidOperationException("Client is not connected to any server");
-    }
-
-    public override async Task OnConnectedAsync()
-    {
-        var httpContext = Context.GetHttpContext();
-
-        if (httpContext is null)
-        {
-            // We simply cannot continue.
-            logger.LogCritical("HttpContext is null - aborting context");
-            Context.Abort();
-            return;
-        }
-
-        var agentId = httpContext.Request.Query["agentId"].ToString();
-        var agentKey = httpContext.Request.Query["agentKey"].ToString();
-
-        if (string.IsNullOrEmpty(agentId) || string.IsNullOrEmpty(agentKey))
-        {
-            logger.LogCritical("AgentId or AgentKey is null - aborting context");
-            Context.Abort();
-            return;
-        }
-
-        if (!Guid.TryParse(agentId, out var serverId))
-        {
-            logger.LogCritical("AgentId is not a Guid - aborting context");
-            Context.Abort();
-            return;
-        }
-
-        var isValidAgent = await serversService.IsValidAgent(serverId, agentKey);
-        if (!isValidAgent)
-        {
-            logger.LogCritical("AgentId or AgentKey is invalid - aborting context");
-            Context.Abort();
-            return;
-        }
-
-        var ip = GetClientIpAddress(httpContext);
-
-        await serverManager.SaveConnection(serverId, Context.ConnectionId);
-        logger.LogDebug("Agent connected from : {AgentId}@{Ip}", agentId, ip);
-
-        await base.OnConnectedAsync();
-    }
-
-    public override async Task OnDisconnectedAsync(Exception? exception)
-    {
-        var clientId = Context.ConnectionId;
-        await serverManager.RemoveConnection(Context.ConnectionId);
-
-        await base.OnDisconnectedAsync(exception);
-    }
-
-    private string GetClientIpAddress(HttpContext context)
-    {
-        var ip = context.Connection?.RemoteIpAddress?.ToString();
-
-        if (string.IsNullOrEmpty(ip))
-        {
-            ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
-        }
-
-        if (string.IsNullOrEmpty(ip))
-        {
-            ip = context.Connection?.RemoteIpAddress?.MapToIPv4().ToString();
-        }
-
-        return ip ?? "Unknown";
     }
 }
---
+++ backend/src/CloudCrafter.Core/SignalR/CloudCrafterBaseHub.cs
@@ -0,0 +1,96 @@
+﻿using CloudCrafter.Core.Interfaces.Domain.Servers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace CloudCrafter.Core.SignalR;
+
+public abstract class CloudCrafterBaseHub(
+    IServersService serversService,
+    ILogger<CloudCrafterBaseHub> logger,
+    ConnectedServerManager serverManager
+) : Hub
+{
+    protected async Task<Guid> GetServerForClient()
+    {
+        var connectionId = Context.ConnectionId;
+        var serverId = await serverManager.GetServerIdForConnectionId(connectionId);
+
+        if (serverId.HasValue)
+        {
+            return serverId.Value;
+        }
+
+        throw new InvalidOperationException("Client is not connected to any server");
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+
+        if (httpContext is null)
+        {
+            // We simply cannot continue.
+            logger.LogCritical("HttpContext is null - aborting context");
+            Context.Abort();
+            return;
+        }
+
+        var agentId = httpContext.Request.Query["agentId"].ToString();
+        var agentKey = httpContext.Request.Query["agentKey"].ToString();
+
+        if (string.IsNullOrEmpty(agentId) || string.IsNullOrEmpty(agentKey))
+        {
+            logger.LogCritical("AgentId or AgentKey is null - aborting context");
+            Context.Abort();
+            return;
+        }
+
+        if (!Guid.TryParse(agentId, out var serverId))
+        {
+            logger.LogCritical("AgentId is not a Guid - aborting context");
+            Context.Abort();
+            return;
+        }
+
+        var isValidAgent = await serversService.IsValidAgent(serverId, agentKey);
+        if (!isValidAgent)
+        {
+            logger.LogCritical("AgentId or AgentKey is invalid - aborting context");
+            Context.Abort();
+            return;
+        }
+
+        var ip = GetClientIpAddress(httpContext);
+
+        await serverManager.SaveConnection(serverId, Context.ConnectionId);
+        logger.LogDebug("Agent connected from : {AgentId}@{Ip}", agentId, ip);
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var clientId = Context.ConnectionId;
+        await serverManager.RemoveConnection(Context.ConnectionId);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    private string GetClientIpAddress(HttpContext context)
+    {
+        var ip = context.Connection?.RemoteIpAddress?.ToString();
+
+        if (string.IsNullOrEmpty(ip))
+        {
+            ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+        }
+
+        if (string.IsNullOrEmpty(ip))
+        {
+            ip = context.Connection?.RemoteIpAddress?.MapToIPv4().ToString();
+        }
+
+        return ip ?? "Unknown";
+    }
+}
