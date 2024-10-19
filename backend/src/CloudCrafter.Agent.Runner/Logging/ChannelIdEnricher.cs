62231d71ab8df9b4e3283c5a1de267093659e25a
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-19 11:25:35 +0200

    Changes

--- backend/src/CloudCrafter.Agent.Runner/Logging/ChannelIdEnricher.cs
+++ backend/src/CloudCrafter.Agent.Runner/Logging/ChannelIdEnricher.cs
@@ -8,39 +8,45 @@ namespace CloudCrafter.Agent.Runner.Logging;
 
 public class ChannelIdEnricher(IServiceProvider serviceProvider) : ILogEventEnricher
 {
-    private static int SEQUENCE;
+    private static int _sequence;
 
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
-        if (logEvent.Properties.TryGetValue("ChannelId", out var value))
+        if (!logEvent.Properties.TryGetValue("ChannelId", out var value))
         {
-            var propertyValue = value.ToString();
-
-            if (Guid.TryParse(propertyValue, out var channelId))
-            {
-                var hubWrapper = serviceProvider.GetRequiredService<IHubWrapper>();
-
-                if (hubWrapper.TypedHubConnection.IsConnected())
-                {
-                    var logMessage = logEvent.RenderMessage();
-                    hubWrapper.TypedHubConnection.InvokeAsync(hub =>
-                        hub.DeploymentOutput(
-                            new DeploymentOutputArgs
-                            {
-                                ChannelId = channelId,
-                                Output = new ChannelOutputLogLine
-                                {
-                                    Date = logEvent.Timestamp.UtcDateTime,
-                                    IsError = logEvent.Level == LogEventLevel.Error,
-                                    Output = logMessage,
-                                    InternalOrder = SEQUENCE,
-                                },
-                            }
-                        )
-                    );
-                    SEQUENCE++;
-                }
-            }
+            return;
         }
+
+        var propertyValue = value.ToString();
+
+        if (!Guid.TryParse(propertyValue, out var channelId))
+        {
+            return;
+        }
+
+        var hubWrapper = serviceProvider.GetRequiredService<IHubWrapper>();
+
+        if (!hubWrapper.TypedHubConnection.IsConnected())
+        {
+            return;
+        }
+
+        var logMessage = logEvent.RenderMessage();
+        hubWrapper.TypedHubConnection.InvokeAsync(hub =>
+            hub.DeploymentOutput(
+                new DeploymentOutputArgs
+                {
+                    ChannelId = channelId,
+                    Output = new ChannelOutputLogLine
+                    {
+                        Date = logEvent.Timestamp.UtcDateTime,
+                        IsError = logEvent.Level == LogEventLevel.Error,
+                        Output = logMessage,
+                        InternalOrder = _sequence,
+                    },
+                }
+            )
+        );
+        _sequence++;
     }
 }
