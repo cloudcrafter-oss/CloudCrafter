ece590af01decf3d9176cc6a2e6779226392d07f
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-22 21:03:06 +0200

    Add migration

---
+++ backend/src/CloudCrafter.Infrastructure/Data/Migrations/20241022190229_AddStateFieldToDeployment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudCrafter.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStateFieldToDeployment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Deployments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "State", table: "Deployments");
+        }
+    }
+}
