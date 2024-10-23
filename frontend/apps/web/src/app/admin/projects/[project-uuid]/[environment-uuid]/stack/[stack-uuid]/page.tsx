5c3fb755b79937df81256e596161584e77472fe9
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-23 18:18:14 +0200

    Moved around log viewer components

--- frontend/apps/web/src/app/admin/projects/[project-uuid]/[environment-uuid]/stack/[stack-uuid]/page.tsx
+++ frontend/apps/web/src/app/admin/projects/[project-uuid]/[environment-uuid]/stack/[stack-uuid]/page.tsx
@@ -1,6 +1,6 @@
 import StackConfigPage from '@/src/components/stack-detail/StackConfigPage'
+import { DeploymentList } from '@/src/components/stack-detail/deployments/deployment-list'
 import {
-	type DeploymentStatusDto,
 	getDeploymentsForStack,
 	getStackDetail,
 } from '@/src/core/__generated__'
@@ -17,24 +17,6 @@ import {
 
 interface PageProps {
 	params: StackRouteParams
-}
-
-const DeploymentStatusBadge = ({ state }: { state: DeploymentStatusDto }) => {
-	return (
-		<span
-			className={`px-2 py-1 rounded-full text-xs font-medium ${
-				state === 'Succeeded'
-					? 'bg-green-100 text-green-800'
-					: state === 'Failed'
-						? 'bg-red-100 text-red-800'
-						: state === 'Running'
-							? 'bg-blue-100 text-blue-800'
-							: 'bg-yellow-100 text-yellow-800'
-			}`}
-		>
-			{state}
-		</span>
-	)
 }
 
 export default async function StackPage({ params }: PageProps) {
@@ -72,31 +54,7 @@ export default async function StackPage({ params }: PageProps) {
 							<div className='space-y-4'>
 								<h2 className='text-2xl font-bold'>Deployments</h2>
 								<ul className='divide-y divide-gray-200'>
-									{deployments.map((deployment) => (
-										<li key={deployment.id} className='py-4'>
-											<div className='flex items-center justify-between'>
-												<div>
-													<p className='text-lg font-semibold'>
-														Manual Deployment
-													</p>
-													<p className='text-sm text-gray-500'>
-														{new Date(deployment.createdAt)
-															.toLocaleString('en-GB', {
-																day: 'numeric',
-																month: 'numeric',
-																year: 'numeric',
-																hour: '2-digit',
-																minute: '2-digit',
-																second: '2-digit',
-																hour12: false,
-															})
-															.replace(',', ' at')}
-													</p>
-												</div>
-												<DeploymentStatusBadge state={deployment.state} />
-											</div>
-										</li>
-									))}
+									<DeploymentList deployments={deployments} />
 								</ul>
 							</div>
 						</TabsContent>
--- frontend/apps/web/src/components/logviewer/ChannelLogViewer.tsx
+++ frontend/apps/web/src/components/logviewer/ChannelLogViewer.tsx
@@ -1,15 +1,43 @@
+import { useGetDeploymentLogsHook } from '@/src/core/__generated__'
 import type { ChannelOutputLogLine } from '@/src/core/__generated__/signal-types/channel-output-log-line'
 import { useWebHub } from '@/src/hooks/useWebHub'
 import { LazyLog } from '@melloware/react-logviewer'
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+} from '@ui/components/ui/sheet'
 
 const formatLogMessage = (message: ChannelOutputLogLine) => {
 	return `${message.date} => ${message.output}`
+}
+
+export const ChannelLogViewerEnhanced = ({
+	channelId,
+	show,
+	onHide,
+}: { channelId: string; show: boolean; onHide: () => void }) => {
+	return (
+		<Sheet open={show} onOpenChange={onHide}>
+			<SheetContent className='min-w-[800px]'>
+				<SheetHeader>
+					<SheetTitle>Logs</SheetTitle>
+				</SheetHeader>
+				<ChannelLogViewer channelId={channelId} />
+			</SheetContent>
+		</Sheet>
+	)
 }
 
 export const ChannelLogViewer = ({ channelId }: { channelId: string }) => {
 	const { messages } = useWebHub({
 		channelId,
 	})
+
+	const { data } = useGetDeploymentLogsHook(channelId)
+
+	console.log(data)
 
 	// iterate over messages and add new line after each so its one big string
 	const logText = messages
