6f0d41d284c033293b31f810231d0cfadd2814ed
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-25 16:44:36 +0200

    Add theme switcher

--- frontend/apps/web/src/layout/components/Sidebar/CloudCrafterSidebar.tsx
+++ frontend/apps/web/src/layout/components/Sidebar/CloudCrafterSidebar.tsx
@@ -1,4 +1,11 @@
 'use client'
+import { Button } from '@ui/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@ui/components/ui/dropdown-menu'
 import {
 	Sidebar,
 	SidebarContent,
@@ -14,11 +21,14 @@ import {
 	GalleryVerticalEnd,
 	LayoutDashboard,
 	Map as MapIcon,
+	Moon,
 	PieChart,
 	Server,
+	Sun,
 	UsersRound,
 } from 'lucide-react'
 import type * as React from 'react'
+import { useTheme } from '../theme-provider'
 import { CloudCrafterNav } from './CloudCrafterNav'
 import { CloudCrafterProjectSwitcher } from './CloudCrafterProjectSwitcher'
 import { CloudCrafterUserNav } from './CloudCrafterUserNav'
@@ -102,6 +112,7 @@ const data = {
 export function CloudCrafterSidebar({
 	...props
 }: React.ComponentProps<typeof Sidebar>) {
+	const { theme, setTheme } = useTheme()
 	return (
 		<Sidebar collapsible='icon' {...props}>
 			<SidebarHeader>
@@ -111,7 +122,29 @@ export function CloudCrafterSidebar({
 				<CloudCrafterNav items={data.navMain} />
 			</SidebarContent>
 			<SidebarFooter>
-				<CloudCrafterUserNav user={data.user} />
+				<div className='flex items-center justify-between px-4 py-2'>
+					<CloudCrafterUserNav user={data.user} />
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant='outline' size='icon'>
+								<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+								<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+								<span className='sr-only'>Toggle theme</span>
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align='end'>
+							<DropdownMenuItem onClick={() => setTheme('light')}>
+								Light
+							</DropdownMenuItem>
+							<DropdownMenuItem onClick={() => setTheme('dark')}>
+								Dark
+							</DropdownMenuItem>
+							<DropdownMenuItem onClick={() => setTheme('system')}>
+								System
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
 			</SidebarFooter>
 			<SidebarRail />
 		</Sidebar>
