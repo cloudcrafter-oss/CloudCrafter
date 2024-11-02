fbce919ebc8fad14ecf8d8cb4ecdaac2c2ceff92
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-02 12:41:17 +0100

    Update site contents

--- frontend/apps/site/components/HomepageHero.tsx
+++ frontend/apps/site/components/HomepageHero.tsx
@@ -1,12 +1,21 @@
 'use client'
-import { ChevronRightIcon } from '@heroicons/react/16/solid'
-import { Menu, X } from 'lucide-react'
+import { Github, Menu, X } from 'lucide-react'
 import Image from 'next/image'
 
 import { useState } from 'react'
 import CloudCrafterLogo from '../assets/logo.png'
 import Screenshot from '../assets/screenshot.webp'
 
+const menuItems = [
+	{
+		url: '/docs',
+		label: 'Docs',
+	},
+	{
+		url: 'https://github.com/cloudcrafter-oss/CloudCrafter',
+		label: 'Github',
+	},
+]
 export const HomepageHero = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 	return (
@@ -50,30 +59,15 @@ export const HomepageHero = () => {
 
 							{/* Desktop Navigation */}
 							<div className='hidden lg:flex lg:gap-x-12'>
-								<a
-									href='#'
-									className='text-sm font-semibold leading-6 text-gray-900'
-								>
-									Products
-								</a>
-								<a
-									href='#'
-									className='text-sm font-semibold leading-6 text-gray-900'
-								>
-									Features
-								</a>
-								<a
-									href='#'
-									className='text-sm font-semibold leading-6 text-gray-900'
-								>
-									About
-								</a>
-								<a
-									href='#'
-									className='text-sm font-semibold leading-6 text-gray-900'
-								>
-									Contact
-								</a>
+								{menuItems.map((item) => (
+									<a
+										key={item.label}
+										href={item.url}
+										className='text-sm font-semibold leading-6 text-gray-900'
+									>
+										{item.label}
+									</a>
+								))}
 							</div>
 
 							{/* Mobile menu button */}
@@ -100,9 +94,11 @@ export const HomepageHero = () => {
 							/>
 							<div className='fixed inset-y-0 right-0 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm'>
 								<div className='flex items-center justify-between'>
-									<img
-										alt='Your Company'
-										src='/api/placeholder/32/32'
+									<Image
+										src={CloudCrafterLogo}
+										alt='CloudCrafter Logo'
+										width={32}
+										height={32}
 										className='h-8 w-auto'
 									/>
 									<button
@@ -116,30 +112,15 @@ export const HomepageHero = () => {
 								</div>
 								<div className='mt-6 flow-root'>
 									<div className='space-y-2 py-6'>
-										<a
-											href='#'
-											className='block px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-										>
-											Products
-										</a>
-										<a
-											href='#'
-											className='block px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-										>
-											Features
-										</a>
-										<a
-											href='#'
-											className='block px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-										>
-											About
-										</a>
-										<a
-											href='#'
-											className='block px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
-										>
-											Contact
-										</a>
+										{menuItems.map((item) => (
+											<a
+												key={item.label}
+												href={item.url}
+												className='block px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
+											>
+												{item.label}
+											</a>
+										))}
 									</div>
 								</div>
 							</div>
@@ -151,37 +132,37 @@ export const HomepageHero = () => {
 				<div className='mx-auto max-w-2xl lg:mx-0 lg:flex-shrink-0 lg:pt-8'>
 					<div className='mt-24 sm:mt-32 lg:mt-16'>
 						<a href='#' className='inline-flex space-x-6'>
-							<span className='rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10'>
-								What's new
+							<span className='rounded-full bg-yellow-100 px-3 py-1 text-sm font-semibold leading-6 text-yellow-800 ring-1 ring-inset ring-yellow-600/20'>
+								🚧 Work In Progress
 							</span>
-							<span className='inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600'>
-								<span>Just shipped v1.0</span>
-								<ChevronRightIcon
-									aria-hidden='true'
-									className='h-5 w-5 text-gray-400'
-								/>
-							</span>
+							{/* <span className='inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600'>
+                                <span>Just shipped v1.0</span>
+                                <ChevronRightIcon
+                                    aria-hidden='true'
+                                    className='h-5 w-5 text-gray-400'
+                                />
+                            </span> */}
 						</a>
 					</div>
 					<h1 className='mt-10 text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl'>
 						Deploy to the cloud with confidence
 					</h1>
 					<p className='mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8'>
-						Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-						lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+						CloudCrafter aims to help you to deploy your Docker stacks easily,
+						wether you're a beginner or an expert.
 					</p>
 					<div className='mt-10 flex items-center gap-x-6'>
 						<a
-							href='#'
+							href='https://github.com/cloudcrafter-oss/CloudCrafter'
 							className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 						>
-							Get started
+							<Github />
 						</a>
 						<a
-							href='#'
+							href='/docs'
 							className='text-sm font-semibold leading-6 text-gray-900'
 						>
-							Learn more <span aria-hidden='true'>→</span>
+							Docs <span aria-hidden='true'>→</span>
 						</a>
 					</div>
 				</div>
