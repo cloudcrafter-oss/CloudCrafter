58d929e67e47f832c6bac7a6756a26a2895cebc8
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-15 18:49:36 +0100

    Fix route

--- frontend/apps/web/src/app/api/config/route.ts
+++ frontend/apps/web/src/app/api/config/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server'
 
-import { backendEnv } from '@/src/core/env/cloudcrafter-env'
-
 export async function GET() {
 	return NextResponse.json({
-		backendUrl: backendEnv.CLOUDCRAFTER_AXIOS_BACKEND_BASEURL,
+		backendUrl: process.env.CLOUDCRAFTER_AXIOS_BACKEND_BASEURL,
 	})
 }
