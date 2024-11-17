015bfb8a3e1f320e119ff09286c10348c4b77a8c
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-17 09:33:23 +0100

    Linting

--- frontend/apps/web/src/app/admin/servers/page.tsx
+++ frontend/apps/web/src/app/admin/servers/page.tsx
@@ -1,6 +1,5 @@
-import { getServers } from '@/src/core/__generated__'
 import { ServersList } from '@/src/core/features/admin/servers/list.tsx'
 
 export default function Page() {
-    return <ServersList />
+	return <ServersList />
 }
