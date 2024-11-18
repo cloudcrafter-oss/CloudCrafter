11d1018851d849d1b68c44dd1485e44ce38202a1
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-18 16:44:35 +0100

    Add basic test

---
+++ backend/tests/CloudCrafter.FunctionalTests/Domain/Servers/MarkServersAsUnknownAfterTimespanTest.cs
@@ -0,0 +1,39 @@
+﻿using CloudCrafter.Core.Commands.Providers;
+using CloudCrafter.Core.Interfaces.Domain.Servers;
+using CloudCrafter.Domain.Entities;
+using CloudCrafter.Infrastructure.Data.Fakeds;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CloudCrafter.FunctionalTests.Domain.Servers;
+
+using static Testing;
+
+public class MarkServersAsUnknownAfterTimespanTest : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldSetServersToUnknownStateAfterTimespan()
+    {
+        // Arrange
+        var server = FakerInstances
+            .ServerFaker.RuleFor(
+                x => x.PingHealthData,
+                f => new ServerPingData
+                {
+                    LastPingAt = DateTime.UtcNow.AddMinutes(-10),
+                    Status = ServerStatusValue.Connected,
+                }
+            )
+            .Generate();
+
+        await AddAsync(server);
+
+        // Act
+        await SendAsync(new ValidateProvidersCommand.Command());
+
+        // Assert
+        var serverAfter = FetchEntity<Server>(f => f.Id == server!.Id)!;
+
+        serverAfter.PingHealthData.Status.Should().Be(ServerStatusValue.Unknown);
+    }
+}
