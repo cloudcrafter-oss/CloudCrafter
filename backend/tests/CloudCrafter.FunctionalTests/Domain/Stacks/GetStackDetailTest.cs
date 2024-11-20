30a75b27827ad8b2d4435e9dd6faa1a5086c69d2
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-20 14:36:36 +0100

    Add tests

---
+++ backend/tests/CloudCrafter.FunctionalTests/Domain/Stacks/GetStackDetailTest.cs
@@ -0,0 +1,55 @@
+﻿using CloudCrafter.Core.Commands.Stacks;
+using CloudCrafter.Infrastructure.Data.Fakeds;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CloudCrafter.FunctionalTests.Domain.Stacks;
+
+using static Testing;
+
+public class GetStackDetailTest : BaseTestFixture
+{
+    [Test]
+    public void ShouldThrowExceptionWhenUserIsNotLoggedIn()
+    {
+        Assert.ThrowsAsync<UnauthorizedAccessException>(
+            async () => await SendAsync(new GetStackDetail.Query { StackId = Guid.NewGuid() }
+            ));
+    }
+    
+    [Test]
+    [Ignore("TODO: ACL check not yet implemented")]
+    public async Task ShoulThrowErrorWhenNoAccessBecauseItDoesNotExists()
+    {
+        await RunAsAdministratorAsync();
+
+        var deploymentId = Guid.NewGuid();
+        var ex = Assert.ThrowsAsync<UnauthorizedAccessException>(
+            async () => await SendAsync(new GetStackDetail.Query { StackId = Guid.NewGuid() })
+        );
+
+        ex.Message.Should().Be($"User does not have access to stack {deploymentId}");
+    }
+
+    [Test]
+    public async Task ShouldReturnStack()
+    {
+        await RunAsAdministratorAsync();
+
+        var project = FakerInstances.ProjectFaker.Generate();
+        await AddAsync(project);
+
+        var environment = FakerInstances.EnvironmentFaker(project).Generate();
+        await AddAsync(environment);
+        
+        var stack = FakerInstances.StackFaker(environment.Id).Generate();
+        await AddAsync(stack);
+        
+        var result = await SendAsync(new GetStackDetail.Query { StackId = stack.Id });
+
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(stack.Id);
+        result.Name.Should().Be(stack.Name);
+    }
+
+}
