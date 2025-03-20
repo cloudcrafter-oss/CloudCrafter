759b9a4ba25d33e9812381518cdf960d5e02bfc5
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-03-20 22:40:08 +0100

    fix: show list of tests that have been ran

--- frontend/apps/web/playwright.config.ts
+++ frontend/apps/web/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
+	reporter: [['list'], ['html']],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -69,7 +69,7 @@ export default defineConfig({
 		command: 'pnpm dev',
 		url: 'http://localhost:3001',
 		reuseExistingServer: !process.env.CI,
-		stdout: 'pipe',
-		stderr: 'pipe',
+		stdout: 'ignore',
+		stderr: 'ignore',
 	},
 })
