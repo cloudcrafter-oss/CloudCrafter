2abcb7bf7a10a85618fefc649ba503fcf88d41a2
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-03-22 16:59:08 +0100

    feat: add migration

---
+++ backend/src/CloudCrafter.Infrastructure/Data/Migrations/20250322155843_AddEnvironmentVariablesToStack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudCrafter.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnvironmentVariablesToStack : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StackEnvironmentVariable",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StackId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    IsSecret = table.Column<bool>(type: "boolean", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(
+                        type: "timestamp with time zone",
+                        nullable: false
+                    ),
+                    UpdatedAt = table.Column<DateTime>(
+                        type: "timestamp with time zone",
+                        nullable: false
+                    ),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModifiedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StackEnvironmentVariable", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StackEnvironmentVariable_Stacks_StackId",
+                        column: x => x.StackId,
+                        principalTable: "Stacks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StackEnvironmentVariable_StackId",
+                table: "StackEnvironmentVariable",
+                column: "StackId"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "StackEnvironmentVariable");
+        }
+    }
+}
