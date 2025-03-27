88c85a581c92af55959b2864e985ed2927b23af1
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-03-27 22:00:49 +0100

    feat: add tests

--- frontend/packages/e2e-testing/src/fixtures/project-fixture.ts
+++ frontend/packages/e2e-testing/src/fixtures/project-fixture.ts
@@ -1,44 +1,23 @@
 import type { APIRequestContext, Page } from '@playwright/test'
-import { test as base } from './playwright-fixture'
+import { test as base, expect } from './playwright-fixture'
 import { UserFixture } from './user-fixture'
 export * from '@playwright/test'
 import { isAxiosError } from 'axios'
-import type { ProjectDto } from '../__generated__'
+import type { ProjectDto, StackCreatedDto } from '../__generated__'
 import {
 	deleteProject as apiDeleteProject,
 	createProject,
+	getProjects,
+	getServers,
+	postCreateStack,
 } from '../__generated__'
-import { ProjectListPage } from '../pages/projects/projects.index'
-import { generateProjectName } from '../utils/fake-data'
+import { generateProjectName, generateStackName } from '../utils/fake-data'
 
 /**
  * Project fixture that extends UserFixture with project creation capabilities
  */
 export class ProjectFixture extends UserFixture {
 	projects: ProjectDto[] = []
-
-	/**
-	 * Delete a project via UI
-	 * @param projectName Name of the project to delete
-	 */
-	async deleteProject(projectName: string): Promise<void> {
-		// Go to projects page
-		await this.page.goto('/admin/projects')
-
-		const projectListPage = ProjectListPage(this.page)
-
-		// Open project actions menu
-		await projectListPage.openProjectActions(projectName)
-
-		// Click delete
-		await projectListPage.actions.delete.click()
-
-		// Confirm deletion
-		await projectListPage.confirmDelete.click()
-
-		// Remove from tracked projects
-		this.projects = this.projects.filter((p) => p.name !== projectName)
-	}
 
 	/**
 	 * Clean up all created projects
@@ -71,8 +50,46 @@ export class ProjectFixture extends UserFixture {
 
 		this.projects.push(result)
 
+		// fetch latest project
+		const projects = await getProjects(
+			{ includeEnvironments: true },
+			this.getApiClientConfig(),
+		)
+		const project = projects.find((p) => p.id === result.id)
+		if (!project) {
+			throw new Error(`Project ${result.name} not found after creation`)
+		}
+
+		return project
+	}
+
+	async fixtureCreateStack(project: ProjectDto): Promise<StackCreatedDto> {
+		expect(project.environments.length).toBeGreaterThan(0)
+		expect(project.environments[0].id).toBeDefined()
+
+		const client = this.getApiClientConfig()
+
+		const servers = await getServers(client)
+		expect(servers.length).toBeGreaterThan(0)
+		expect(servers[0].id).toBeDefined()
+
+		const result = await postCreateStack(
+			{
+				environmentId: project.environments[0].id,
+				name: generateStackName(),
+				gitRepository: 'https://github.com/cloudcrafter-oss/demo-examples',
+				serverId: servers[0].id,
+			},
+			client,
+		)
+
 		return result
 	}
+}
+
+interface StackDetails {
+	stack: StackCreatedDto
+	project: ProjectDto
 }
 
 /**
@@ -83,6 +100,7 @@ export const test = base.extend<{
 	page: Page
 	projectFixture: ProjectFixture
 	request: APIRequestContext
+	stack: StackDetails
 }>({
 	project: async ({ browser, page, request }, use) => {
 		// Create project fixture
@@ -109,5 +127,15 @@ export const test = base.extend<{
 
 		// Make fixture available for use in tests
 		await use(projectFixture)
+	},
+
+	stack: async ({ projectFixture, project }, use) => {
+		// Make fixture available for use in tests
+
+		const result: StackDetails = {
+			project,
+			stack: await projectFixture.fixtureCreateStack(project),
+		}
+		await use(result)
 	},
 })
