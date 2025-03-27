b181577a64ca897a7a978fb6e5eb1a62018dee1e
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-03-27 10:29:36 +0100

    feat: no mobile ports

--- frontend/packages/e2e-testing/playwright.config.ts
+++ frontend/packages/e2e-testing/playwright.config.ts
@@ -53,15 +53,15 @@ export default defineConfig({
 			use: { ...devices['Desktop Safari'] },
 		},
 
-		/* Test against mobile viewports. */
-		{
-			name: 'Mobile Chrome',
-			use: { ...devices['Pixel 5'] },
-		},
-		{
-			name: 'Mobile Safari',
-			use: { ...devices['iPhone 12'] },
-		},
+		/* Test against mobile viewports, not for now. */
+		// {
+		// 	name: 'Mobile Chrome',
+		// 	use: { ...devices['Pixel 5'] },
+		// },
+		// {
+		// 	name: 'Mobile Safari',
+		// 	use: { ...devices['iPhone 12'] },
+		// },
 	],
 
 	/* Run your local dev server before starting the tests */
