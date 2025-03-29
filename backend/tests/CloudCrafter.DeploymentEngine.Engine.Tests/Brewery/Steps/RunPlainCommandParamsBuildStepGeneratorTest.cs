4c92818350d296dd8eb08bc54a7dcc19ba9d53ab
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-03-29 11:54:02 +0100

    feat: add more tests

---
+++ backend/tests/CloudCrafter.DeploymentEngine.Engine.Tests/Brewery/Steps/RunPlainCommandParamsBuildStepGeneratorTest.cs
@@ -0,0 +1,32 @@
+﻿using CloudCrafter.Agent.Models.Recipe;
+using CloudCrafter.DeploymentEngine.Engine.Brewery.Steps;
+using FluentAssertions;
+
+namespace CloudCrafter.DeploymentEngine.Engine.Tests.Brewery.Steps;
+
+public class RunPlainCommandParamsBuildStepGeneratorTest
+    : StepBaseTest<RunPlainCommandParamsBuildStepGenerator>
+{
+    [TestCase(true)]
+    [TestCase(false)]
+    public void ShouldGenerateBuildStep(bool allowFailure)
+    {
+        // Arrange
+        var options = new RunPlainCommandParamsBuildStepGenerator.Args
+        {
+            Command = "some-command",
+            AllowFailure = allowFailure,
+        };
+        var generator = new RunPlainCommandParamsBuildStepGenerator(options);
+
+        // Act
+        var buildStep = generator.Generate();
+
+        // Assert
+        buildStep.Type.Should().Be(DeploymentBuildStepType.RunPlainCommand);
+        buildStep.Params.Should().ContainKeys("command", "allowFailure");
+
+        buildStep.Params["command"].Should().Be("some-command");
+        buildStep.Params["allowFailure"].Should().Be(allowFailure);
+    }
+}
